e54df8441228e72e1f43dbf544ee63efaf338763
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2025-01-26 23:14:41 -0500

    Apply a nested loop by completing the chess board Challenge

--- Demo_Folder/demo-code.js
+++ Demo_Folder/demo-code.js
@@ -4,8 +4,8 @@
 for (let triangle = "#"; triangle.length <= 7; triangle += "#") {
   console.log(triangle);
 }
-
 /* Fizz Buzz Exercise */
+
 for (let index = 1; index <= 100; index++) {
   if (index % 3 === 0 && index % 5 === 0) {
     console.log("FizzBuzz");
@@ -17,3 +17,21 @@ for (let index = 1; index <= 100; index++) {
     console.log(index);
   }
 }
+/**
+ * @param chessboard  Challenge
+ Write a program that creates a string that represents an 8 x 8 grid, using new-line characters to separate  lines . At each position of the grid there ia either a space or "◼️", "◽️", which should form a chessboard.
+ */
+let size  = 8;
+let board = ""
+
+for (let index = 0; index < size; index++) {
+for (let index2 = 0; index2 < size ;  index2++) {
+  if ((index + index2) % 2 === 0) {
+    board += "◽️";
+  }else{
+board += "◼️";
+  };
+}; 
+board += "\n";
+};
+console.log(board);
